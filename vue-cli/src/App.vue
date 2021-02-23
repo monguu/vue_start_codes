55f54f2c74c2e70361fb080a4ded8bc8e4b1c534
kimkwongu <kwongu14@gmail.com>
2021-02-23 17:58:45 +0900

    axios 구현중

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">ID: </label>
+        <input id="username" type="text" v-model="username">
+      </div>
+      <div>
+        <label for="passwrod">PW: </label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      value:"",
+    }
+  },
+  methods: {
+    submitForm() {
+      // event.preventDefault(); //새로고침 막는 이벤트
+      console.log(this.username, this.password)
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      axios.post();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
